2be4d19d6b5f256d09ffbf7775fd3fa11f7d987c
Gonzalo Martínez Embil <gonzalomembil@gmail.com>
2026-01-10 18:48:37 +0100

    Add DOMContentLoaded event listener to script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Página cargada correctamente');
+    
+    // Aquí puedes añadir funcionalidad JavaScript en el futuro
+});
